bc7d50022d74222b752ef6177b9c43c8fb7004e5
KNIF <admin@knif.dev>
2020-04-15 17:48:04 +0200

    Added Say command

---
+++ commands/say.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'say',
+    description: 'Says something as the bot.',
+    usage: '[message]',
+    aliases: ['embed'],
+    execute: async (client, message, args) => {
+        message.channel.send({
+            embed: {
+                description: args.join(' '),
+                color: 3835585,
+                footer: {
+                    text: 'Requested by ' + message.author.tag,
+                    icon_url: message.author.displayAvatarURL,
+                },
+                timestamp: new Date(),
+            },
+        });
+    },
+};
